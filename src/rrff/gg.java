dc947647e661c51740665d87131487781cfffd89
zhangsunqiang <zhsunq@163.com>
2017-07-07 11:56:35 +0800

    fdasdfasd

--- src/rrff/gg.java
+++ src/rrff/gg.java
@@ -16,7 +16,8 @@ public class gg {
         System.out.println("ni hao a  bai chi!!");//dddgfgfhdgdfgdfg
 		System.out.println("yuyuyuyuyu");//dddddddddf
 		System.out.println("shuxue hao a ");//bie dou bi l 
-        System.out.println("zuihoudexiugail!!!");//ssssssrrrrr
-		int i =77;
+
+		System.out.println("zuihoudexiugail!!!");//ssrrr
+		int i =7;
    }
 }
