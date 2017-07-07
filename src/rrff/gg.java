9770ca68f5835401e60fe9f080aa823be7d5d8ee
zhangsunq <zhsunq@163.com>
2017-07-07 10:51:14 +0800

    Update gg.java

--- src/rrff/gg.java
+++ src/rrff/gg.java
@@ -4,7 +4,7 @@ public class gg {
 	//sdxfgsdfvzxc 谢谢你 你好啊！！！
 		///zzxczzxc//6yh5tg5tff5tf  hello world!!!fyxbgdszxxxczxxcdda
 	public static void main(String[] args) {//jiushixiajibawan
-		System.out.println("hello world!!");//sssszaaasdaas
+		System.out.println("hello world!!");//sssszaaasdaas你好 世界！！！
 		System.out.println("ssssssss");//fffffffyfyff
 	}
 }
