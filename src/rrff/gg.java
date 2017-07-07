aa228f82d0a807ee1b860b22aaff9bc340d95373
zhangsunqiang <zhsunq@163.com>
2017-07-07 10:48:47 +0800

    ddd

--- src/rrff/gg.java
+++ src/rrff/gg.java
@@ -3,8 +3,8 @@ package rrff;
 public class gg {
 	//sdxfgsdfvzxc 谢谢你 你好啊！！！
 		///zzxczzxc//6yh5tg5tff5tf  hello world!!!fyxb
-	public static void main(String[] args) {
+	public static void main(String[] args) {//jiushixiajibawan
 		System.out.println("hello world!!");//sssszaaasdaas
-		System.out.println("ssssssss");
+		System.out.println("ssssssss");//fffffffyfyff
 	}
 }
