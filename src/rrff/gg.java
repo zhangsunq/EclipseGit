a58db48d0cf346b88cfcb9358f0d0d6ab1a721c7
zhangsunqiang <zhsunq@163.com>
2017-07-07 10:22:13 +0800

    zx

--- src/rrff/gg.java
+++ src/rrff/gg.java
@@ -1,6 +1,9 @@
 package rrff;
 
 public class gg {
-//sdxfgsdfvzxc 谢谢你 你好啊！！！
-	///zzxczzxc//6yh5tg5tff5tf
+        //sdxfgsdfvzxc 璋㈣阿浣� 浣犲ソ鍟婏紒锛侊紒
+	///zzxczzxc//6yh5tg5tff5tf  hello world!!!fyxb
+	public static void main(String[] args) {
+		System.out.println("hello world!!");
+	}
 }
