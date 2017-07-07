64f5cfd4f45a389efb8da9aab1d44050395ab3fd
zhangsunqiang <zhsunq@163.com>
2017-07-07 10:25:03 +0800

    xxx

--- src/rrff/gg.java
+++ src/rrff/gg.java
@@ -4,6 +4,6 @@ public class gg {
         //sdxfgsdfvzxc 璋㈣阿浣� 浣犲ソ鍟婏紒锛侊紒
 	///zzxczzxc//6yh5tg5tff5tf  hello world!!!fyxb
 	public static void main(String[] args) {
-		System.out.println("hello world!!");
+		System.out.println("hello world!!");//sssszaaasdaas
 	}
 }
